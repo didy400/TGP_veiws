3762ea9a167ddc23affc2199a744746a507e8aa0
didy400 <didy.and@gmail.com>
2020-05-05 09:34:53 +0200

    correctifs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#accueil'
   get '/contact', to: 'contact#information'
   get '/team', to: 'team#presentation'
-  get 'welcome/:first_name', to: 'welcome#get_user_name'
+  get 'welcome/:first_name', to: 'welcome#hello'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
